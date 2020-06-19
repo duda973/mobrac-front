66ba68e3b21ac7fa0a3f1d6a44c53d102ab72f65
Dragutin Todorovic <dragutin.todorovic@rgc.rs>
2020-06-19 19:15:15 +0200

    Fixed bugs

--- src/app/edit-song/edit-song.page.ts
+++ src/app/edit-song/edit-song.page.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { SongService } from './../shared/song.service';
+import { Component, OnInit } from "@angular/core";
+import { SongService } from "./../shared/song.service";
 import { ActivatedRoute, Router } from "@angular/router";
 import { FormGroup, FormBuilder } from "@angular/forms";
 
 @Component({
-  selector: 'app-edit-song',
-  templateUrl: './edit-song.page.html',
-  styleUrls: ['./edit-song.page.scss'],
+  selector: "app-edit-song",
+  templateUrl: "./edit-song.page.html",
+  styleUrls: ["./edit-song.page.scss"],
 })
 export class EditSongPage implements OnInit {
-
   updateSongForm: FormGroup;
   id: any;
 
@@ -19,27 +18,26 @@ export class EditSongPage implements OnInit {
     private router: Router,
     public fb: FormBuilder
   ) {
-    this.id = this.actRoute.snapshot.paramMap.get('id');
+    this.id = this.actRoute.snapshot.paramMap.get("id");
   }
 
   ngOnInit() {
     this.getSongData(this.id);
     this.updateSongForm = this.fb.group({
-      name: [''],
-      release_date: [''],
-      album_name: [''],
-      lyrics: ['']
-    })
+      name: [""],
+      release_date: [""],
+      album_name: [""],
+      lyrics: [""],
+    });
   }
 
   getSongData(id) {
-    this.songAPI.getSong(id).subscribe(res => {
+    this.songAPI.getSong(id).subscribe((res) => {
       this.updateSongForm.setValue({
-        name: res['name'],
-        release_date: res['release_date'],
-        album_name: res['album_name'],
-        lyrics: res['lyrics']
-
+        name: res["name"],
+        release_date: res["release_date"],
+        album_name: res["album_name"],
+        lyrics: res["lyrics"],
       });
     });
   }
@@ -48,13 +46,13 @@ export class EditSongPage implements OnInit {
     if (!this.updateSongForm.valid) {
       return false;
     } else {
-      this.songAPI.updateSong(this.id, this.updateSongForm.value)
+      this.songAPI
+        .updateSong(this.id, this.updateSongForm.value)
         .subscribe((res) => {
-          console.log(res)
+          console.log(res);
           this.updateSongForm.reset();
-          this.router.navigate(['/home']);
-        })
+          this.router.navigate(["/home"]);
+        });
     }
   }
-
 }
